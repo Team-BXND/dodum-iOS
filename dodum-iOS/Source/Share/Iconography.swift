dd446690976e5ec55fb57a47ee439eb30d4c0877
gituser <danjee2009@gmail.com>
2025-09-10 20:39:55 +0900

    MainView, Iconography

---
+++ dodum-iOS/Source/Share/Iconography.swift
@@ -0,0 +1,18 @@
+//
+//  Iconography.swift
+//  dodum-iOS
+//
+//  Created by maple on 9/10/25.
+//
+
+import SwiftUI
+
+struct Iconography: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Iconography()
+}
---
+++ dodum-iOS/Source/Util/ServerURL.swift
@@ -0,0 +1,18 @@
+//
+//  ServerURL.swift
+//  dodum-iOS
+//
+//  Created by maple on 9/10/25.
+//
+
+import SwiftUI
+
+struct ServerURL: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ServerURL()
+}
